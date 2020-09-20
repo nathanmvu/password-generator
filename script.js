29d85bc5a799095a3a6d2043e113517a38296ad5
Nathan Vu <nathanvu@Nathans-MacBook-Pro.local>
2020-09-19 18:53:41 -0700

    Finished password generator and added content to README.md

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Array of special characters
+// Arrays of special characters, upper/lowercase characters, and numbers
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "]", "{", "}", ";", ":", "'", "/", "?", "<",">", ",", "."];
-// Arrays of upper and lowercase letters
 var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// Array of numbers
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Initializing confirm booleans
@@ -22,19 +20,32 @@ var passwordLength = 0;
 var password = '';
 
 function passwordPrompts() {
-  containsUpper = confirm("Contains uppercase letters?");
-  containsLower = confirm("Contains lowercase letters?");
-  containsNum = confirm("Contains numbers?");
-  containsSpecial = confirm("Contains special characters?");
-  var check = 0;
-  while(check == 0) {
+  // Boolean variable to check that prompts are all true
+  var promptsAnswered = false;
+  // Loop to check that at least one parameter is selected
+  while(promptsAnswered == false) {
+    containsUpper = confirm("Contains uppercase letters?");
+    containsLower = confirm("Contains lowercase letters?");
+    containsNum = confirm("Contains numbers?");
+    containsSpecial = confirm("Contains special characters?");
+
+    if(containsUpper || containsLower || containsNum || containsSpecial) {
+      promptsAnswered = true;
+    } else {
+      alert("Password needs to have at least one of the parameters.");
+    }
+  }
+  
+  // Boolean variable to check that the given password length is adequate
+  var checkLength = false;
+  while(checkLength == false) {
     var lengthPrompt = prompt("How many characters long should the password be? (Between 8 and 128 Characters)");
     passwordLength = parseInt(lengthPrompt);
     //Check to see if inputted length is within our parameters
-    if(passwordLength < 8 || passwordLength > 128) {
+    if(passwordLength < 8 || passwordLength > 128 || lengthPrompt == "") {
       alert("Please enter a number between 8 and 128");
     } else {
-      check++;
+      checkLength = true;
       writePassword();
     }
   } 
@@ -42,23 +53,24 @@ function passwordPrompts() {
 }
 
 function generatePassword() {
-  alert(containsUpper);
+  // Clear password before setting a new one
+  password = '';
   // Initializing variable to decide which arrays to use for password
   var whichArray;
   for(var i = 0; i < passwordLength; i++) {
     var whichArray = Math.floor(Math.random() * 4);
     // Setting special character for password
-    // Randomly select index number for array of 28 special characters
+    // Randomly select index number for array of 27 special characters
     if(whichArray == 0 && containsSpecial== true) {
       var specialIndex = Math.floor(Math.random() * 27);
       password += specialChar[specialIndex];
     // Randomly select index number for array of 26 uppercase characters
     } else if (whichArray == 1 && containsUpper==true) {
-      var upperIndex = Math.floor(Math.random() * 25);
+      var upperIndex = Math.floor(Math.random() * 26);
       password += uppercase[upperIndex];
     // Randomly select index number for array of 26 lowercase characters
     } else if (whichArray == 2 && containsLower==true) {
-      var lowerIndex = Math.floor(Math.random() * 25);
+      var lowerIndex = Math.floor(Math.random() * 26);
       password += lowercase[lowerIndex];
     // Randomly select index number for array of 10 number characters
     } else if (whichArray == 3 && containsNum==true) {
@@ -76,7 +88,6 @@ function generatePassword() {
 // Write password to the #password input
 function writePassword() {
   var generatedPassword = generatePassword();
-  alert(generatedPassword);
   var passwordText = document.querySelector("#password");
   passwordText.value = generatedPassword;
 
@@ -84,4 +95,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", passwordPrompts);
-//generateBtn.addEventListener("click", writePassword);
